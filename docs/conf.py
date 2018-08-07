4375f2f0890eea71ec10131f161e3ac8a3251840
Davide Gessa <gessadavide@gmail.com>
2018-08-07 09:42:50 +0200

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,11 @@ except ImportError:
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/index.rst
+++ docs/index.rst
@@ -1,4 +1,4 @@
-Welcome to tezpy's documentation!
+TezPy documentation
 =================================
 
 .. toctree::
